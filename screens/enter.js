70c727328f4507ca77d15eed459678340d464459
alexander-korotkiy <korotliy.al@gmail.com>
2019-06-24 00:47:37 +0300

    Changed screens

--- screens/enter.js
+++ screens/enter.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import  {Container, Content, StyleProvider } from 'native-base';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Container, Content, StyleProvider} from 'native-base';
 
 import HeaderEnter from '../components/HeaderEnter.js'
 import FooterComponent from '../components/FooterComponent.js'
 import EnterBody from '../components/EnterBody.js'
 
 
-
 export default class Enter extends React.Component {
 
-  render() {
-    return (
-  
-      <Container>
-        
-        <HeaderEnter />
-        <EnterBody />
-        <FooterComponent />
-        
-             
-      </Container>
-    
-    )
-  }
+    static navigationOptions = {
+        title: 'Вход',
+    };
 
-  }
+    render() {
+        return (
+
+            <Container>
+
+                <HeaderEnter/>
+                <EnterBody/>
+                <FooterComponent/>
+
+
+            </Container>
+
+        )
+    }
+
+}
 
--- screens/registration.js
+++ screens/registration.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import  {Container, Content, StyleProvider } from 'native-base';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Container, Content, StyleProvider} from 'native-base';
 
 import HeaderRegistration from '../components/HeaderRegistration.js'
 import FooterComponent from '../components/FooterComponent.js'
 import RegistrationBody from '../components/RegistrationBody.js'
 
 
-
 export default class Registration extends React.Component {
 
-  render() {
-    return (
-  
-      <Container>
-        
-        <HeaderRegistration />
-        <RegistrationBody />
-        <FooterComponent />
-        
-             
-      </Container>
-    
-    )
-  }
+    static navigationOptions = {
+        title: 'Регистарция',
+    };
 
-  }
+    render() {
+        return (
+
+            <Container>
+
+                <HeaderRegistration/>
+                <RegistrationBody/>
+                <FooterComponent/>
+
+
+            </Container>
+
+        )
+    }
+
+}
 
